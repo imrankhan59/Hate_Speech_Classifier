9306cae5c7d47a9fb81488f8bb02754290457bb0
unknown <ik9449080@gmail.com>
2025-09-18 01:25:39 +0500

    modified: src/constant/__init__.py remove extra code

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -2,8 +2,6 @@ import os
 
 # Common Constants
 ARTIFACT_DIR = "artifacts"
-BUCKET_NAME = "GCP_BUCKET_NAME"
-ZIP_FILE_NAME = "dataset.zip" # after downoad the zip file, it will be stored in this path
 LABEL = "label"  # column name
 TWEET = "tweet"  # column name
 
